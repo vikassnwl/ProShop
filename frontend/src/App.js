dd0b552b5fa241324dca7abaf0e6bf8480ea3b16
vikassnwl <vikassnwl@gmail.com>
2021-04-26 19:24:28 +0530

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import products from "../../src/products";
+import Product from "../components/Product";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default HomeScreen;
